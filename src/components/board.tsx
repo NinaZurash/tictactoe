e23c63ed4a15ab6ca8480efa1141e6ffb842c60f
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-10-08 12:20:25 +0200

    add tictac

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -16,6 +16,7 @@ export default function Board({ dimension }: BoardProps) {
       return newValues
     })
     setTictac(tictac === 'X' ? '0' : 'X')
+    console.log(tictac)
   }
   return (
     <div>
