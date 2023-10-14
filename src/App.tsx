a89253d73e2ba0eed9b51f9aeca365c70c3bc5d3
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-10-14 14:07:20 +0200

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Confetti from "react-confetti";
 import Board from "./components/board";
 import { useState } from "react";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 export type GameStatus = "X" | "0" | "draw" | "playing";
 
@@ -14,17 +16,22 @@ export default function App() {
   const visible = gameStatus === "playing" ? "invisible" : "visible";
 
   return (
-    <div className="min-h-screen flex justify-center flex-col  gap-8 items-center">
-      {["X", "0"].includes(gameStatus) && <Confetti />}
-      <h1 className="text-4xl  font-medium">TicTacToe</h1>
-      <div className={visible}>
-        {gameStatus === "draw" ? "It's a draw" : ` Winner is ${gameStatus}!`}
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen flex justify-center flex-col  gap-8 items-center">
+        <div className="absolute top-0 right-0 mt-4 mr-4">
+          <ModeToggle />
+        </div>
+        {["X", "0"].includes(gameStatus) && <Confetti />}
+        <h1 className="text-4xl  font-medium">TicTacToe</h1>
+        <div className={visible}>
+          {gameStatus === "draw" ? "It's a draw" : ` Winner is ${gameStatus}!`}
+        </div>
+        <Board
+          dimension={3}
+          winner={gameStatus}
+          updateGameStatus={handleGameUpdateStatus}
+        />
       </div>
-      <Board
-        dimension={3}
-        winner={gameStatus}
-        updateGameStatus={handleGameUpdateStatus}
-      />
-    </div>
+    </ThemeProvider>
   );
 }
